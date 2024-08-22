d79a7bc8bbfb2a61ef241c40c5393383a86e691d
Shiffff <alexis.privat@live.fr>
2024-08-22 18:29:19 +0200

    (css):css on multple files

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -26,8 +26,9 @@ const SignUp = () => {
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
-        <h1 className="text-3x1 font-semibold text-center text-gray-300">
-          Sign Up <span className="text-blue-500">App</span>
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
+          Sign Up <br />
+          <span className="text-blue-500">Your Car Your Way App</span>
         </h1>
         <form onSubmit={handleSubmit}>
           <div>
